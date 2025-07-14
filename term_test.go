6b456ddcb8fd45aa863bdee440612b333b86d9bf
abdugaffor2004 <abdughaffar7104@gmail.com>
2025-07-14 14:58:49 +0300

    feat: add more tests

--- term_test.go
+++ term_test.go
@@ -9,6 +9,7 @@ import (
 )
 
 func TestInit(t *testing.T) {
+
 	tests := []struct {
 		name           string
 		noColorEnv     string
@@ -60,6 +61,94 @@ func TestInit(t *testing.T) {
 			assert.Equal(t, tc.wantNoColor, NoColor)
 			assert.Equal(t, tc.wantForceColor, ForceColor)
 
+			tc.reset()
+		})
+	}
+}
+
+func TestSupportsColor(t *testing.T) {
+	tests := []struct {
+		name         string
+		termEnv      string
+		colorTermEnv string
+		want         bool
+		setup        func()
+		reset        func()
+	}{
+		{
+			name: "TERM=dumb",
+			want: false,
+			setup: func() {
+				_ = os.Setenv("TERM", "dumb")
+			},
+			reset: func() {
+				_ = os.Unsetenv("TERM")
+			},
+		},
+		{
+			name:    "TERM is empty",
+			termEnv: "",
+			want:    false,
+			setup: func() {
+				_ = os.Setenv("TERM", "")
+			},
+			reset: func() {
+				_ = os.Unsetenv("TERM")
+			},
+		},
+		{
+			name:    "TERM=xterm-256color",
+			termEnv: "xterm-256color",
+			want:    true,
+			setup: func() {
+				_ = os.Setenv("TERM", "xterm-256color")
+			},
+			reset: func() {
+				_ = os.Unsetenv("TERM")
+			},
+		},
+		{
+			name:    "TERM=screen",
+			termEnv: "screen",
+			want:    true,
+			setup: func() {
+				_ = os.Setenv("TERM", "screen")
+			},
+			reset: func() {
+				_ = os.Unsetenv("TERM")
+			},
+		},
+		{
+			name:    "TERM contains 'xterm'",
+			termEnv: "my-xterm-compatible",
+			want:    true,
+			setup: func() {
+				_ = os.Setenv("TERM", "xterm")
+			},
+			reset: func() {
+				_ = os.Unsetenv("TERM")
+			},
+		},
+		{
+			name:    "TERM=linux",
+			termEnv: "linux",
+			want:    true,
+			setup: func() {
+				_ = os.Setenv("TERM", "linux")
+
+			},
+			reset: func() {
+				_ = os.Unsetenv("TERM")
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.setup()
+			result := SupportsColor()
+
+			assert.Equal(t, tc.want, result)
 			tc.reset()
 		})
 	}
